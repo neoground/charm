df7102916f246179714d5ffa526b7293eb258a93
Sven Reifschneider <9088619+neoground@users.noreply.github.com>
2018-10-29 22:58:46 +0100

    Added postInit hook to app

--- src/Vivid/Kernel/Handler.php
+++ src/Vivid/Kernel/Handler.php
@@ -115,6 +115,9 @@ class Handler
         // Init the whole system
         $this->initSystem();
 
+        // App's post init hook
+        $this->getModule('App')->postInit();
+
         // Route + Output!
         $this->output();
 
