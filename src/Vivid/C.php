6dab8ff6b042bc946210d21f32afde35bd4f564d
Sven Reifschneider <9088619+neoground@users.noreply.github.com>
2019-07-15 22:42:22 +0200

    Added C

---
+++ src/Vivid/C.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This file contains the C main access class
+ */
+
+namespace Charm\Vivid;
+
+/**
+ * Class C
+ *
+ * Alternative syntax for Charm class calls
+ *
+ * @see Charm
+ *
+ * @package Charm\Vivid
+ */
+class C extends Charm { }
