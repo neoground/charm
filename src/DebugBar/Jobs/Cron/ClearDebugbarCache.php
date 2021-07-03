18853a8b4f991b57b76dfd8e1351484e6f78665f
Sven Reifschneider <9088619+neoground@users.noreply.github.com>
2021-07-03 09:55:28 +0200

    ✨ Added automatic debugbar cache clean up cron job

---
+++ src/DebugBar/Jobs/Cron/ClearDebugbarCache.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * This file contains the ClearDebugbarCache cron job
+ */
+
+namespace Charm\DebugBar\Jobs\Cron;
+
+use Carbon\Carbon;
+use Charm\Crown\Cronjob;
+use Charm\Vivid\C;
+use Charm\Vivid\Charm;
+
+/**
+ * Class ClearDebugbarCache
+ *
+ * Cleaning up the debug bar cache regulary and remove files older than 2 days
+ *
+ * @package Charm\DebugBar\Jobs\Cron
+ */
+class ClearDebugbarCache extends Cronjob
+{
+    /**
+     * Cron job configuration
+     */
+    protected function configure()
+    {
+        $this->setName('Cleaning up the debug bar cache')
+            ->runDaily(4);
+    }
+
+    /**
+     * Run that job.
+     *
+     * @return bool
+     */
+    public function run()
+    {
+        $path = C::Storage()->getCachePath() . DS . 'debugbar';
+        $now = time();
+
+        if(file_exists($path)) {
+            $files = scandir($path);
+            foreach($files as $file) {
+                if (is_file($file)) {
+                    // When file is older than 48 hours, delete it
+                    if ($now - filemtime($file) >= 60 * 60 * 48) {
+                        unlink($file);
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+}
