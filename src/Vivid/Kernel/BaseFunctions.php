873dc8746ea44927dc9ce822c217befc3b08c182
Sven Reifschneider <9088619+neoground@users.noreply.github.com>
2024-07-11 23:51:04 +0200

    🔥 Remove polyfill for `apache_request_headers()` since we now have an own module `C::Header()`

--- src/Vivid/Kernel/BaseFunctions.php
+++ src/Vivid/Kernel/BaseFunctions.php
@@ -80,36 +80,3 @@ if (!function_exists('cCurrentUrl')) {
     }
 
 }
-
-/**
- * Manually set apache_request_headers. Needed if php doesn't run as apache module
- * Source: https://secure.php.net/manual/en/function.apache-request-headers.php
- */
-if (!function_exists('apache_request_headers')) {
-
-    /**
-     * Fetch all HTTP request headers
-     *
-     * @return array|false
-     */
-    function apache_request_headers(): bool|array
-    {
-        $arh = [];
-        $rx_http = '/\AHTTP_/';
-        foreach ($_SERVER as $key => $val) {
-            if (preg_match($rx_http, $key)) {
-                $arh_key = preg_replace($rx_http, '', $key);
-                // do some nasty string manipulations to restore the original letter case
-                // this should work in most cases
-                $rx_matches = explode('_', $arh_key);
-                if (count($rx_matches) > 0 and strlen($arh_key) > 2) {
-                    foreach ($rx_matches as $ak_key => $ak_val) $rx_matches[$ak_key] = ucfirst($ak_val);
-                    $arh_key = implode('-', $rx_matches);
-                }
-                $arh[$arh_key] = $val;
-            }
-        }
-        return ($arh);
-    }
-
-}
