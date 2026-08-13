7a0e89be1182717b97873902edbc1cb40dddd031
Sarah Robin <9088619+neoground@users.noreply.github.com>
2026-08-13 11:19:37 +0200

    🚀 Update composer packages, version change

--- src/Vivid/C.php
+++ src/Vivid/C.php
@@ -60,7 +60,7 @@ use Charm\Vivid\Kernel\Interfaces\ModuleInterface;
 class C
 {
     /** @var string the version of charm */
-    public const string VERSION = "3.8.4";
+    public const string VERSION = "3.9";
 
     /**
      * Get a loaded module
